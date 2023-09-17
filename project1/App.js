90c04d396411664eb1665e8c4613a8a14a1d7475
udikakumari2 <udikakumari5@gmail.com>
2023-09-16 20:35:53 -0700

    add

---
+++ project1/App.js
@@ -0,0 +1,31 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text ,useColorScheme} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/Home';
+import Signup from './src/Signup';
+import Login from './src/Login';
+import content from './src/content';
+import splash from './src/splash';
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="splash" component={splash} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="content" component={content} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
